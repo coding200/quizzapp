9e40c04fa919b622a5e5d267d6dd5af25653b5fa
coding200 <79961384+coding200@users.noreply.github.com>
2021-04-10 13:13:50 +0530

    Delete src directory

--- src/common/exceptions.ts
+++
@@ -1,6 +0,0 @@
-export enum ErrorCode {
-  ACCESS_DENIED = 'Access Denied',
-  TASK_NOT_PRESENT = 'No Task for this ID',
-  INVALID_CREDENTIALS = 'Invalid Credentials',
-  SAME_USERNAME = 'Username Already present',
-}
