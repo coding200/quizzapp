1e28cb259ddcfbf23646b09a0e861dce71ddd26f
coding200 <79961384+coding200@users.noreply.github.com>
2021-04-21 19:10:54 +0530

    Delete src directory

--- src/common/exceptions.ts
+++
@@ -1,8 +0,0 @@
-export enum ErrorCode {
-  ACCESS_DENIED = 'Access Denied',
-  NOT_PRESENT = 'Enter valid ID',
-  INVALID_CREDENTIALS = 'Invalid Credentials',
-  SAME_USERNAME = 'Username Already present',
-  SAME_PHONE = 'Phone Already Present',
-  SAME_EMAIL = 'Email Already Present',
-}
--- src/participants/dto/participants.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNotEmpty } from 'class-validator';
-import { InputType, Field } from '@nestjs/graphql';
-
-@InputType()
-export class CreatePartDto {
-  @Field()
-  @IsNotEmpty()
-  quiz_id: number;
-}
